be65f5354374b8f72dac9ba01fc2c6a67d31fc1a
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2019-05-15 11:57:33 -0700

    event binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,19 +5,27 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <h1 [innerHTML]="title"></h1>
-      <h1>{{ title }}</h1>
-      <img [src]="logo">
-      <input type="text" [value]="name">
+      <button (click)="handleClick()">
+        Change name
+      </button>
+      <input 
+        type="text" 
+        [value]="name"
+        (input)="handleInput($event)"
+        (blur)="handleBlur($event)">
       <div>{{ name }}</div>
     </div>
   `
 })
 export class AppComponent {
-  title: string;
   name: string = 'Todd';
-  logo: string = 'img/logo.svg';
-  constructor() {
-    this.title = 'Ultimate Angular';
+  handleClick() {
+    this.name = 'Motto';
+  }
+  handleInput(event: any) {
+    this.name = event.target.value;
+  }
+  handleBlur(event: any) {
+    this.name = event.target.value;
   }
 }
