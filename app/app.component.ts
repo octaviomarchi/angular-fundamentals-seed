37d83b81e34000a7bbdbb5d1a28d38d2d88a0b28
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2019-05-16 17:05:11 -0700

    ngIf, * syntax and ng-template
    ng template only for angular 5 and above

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,17 +5,27 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <button (click)="handleClick(username.value)">
-        Get value
-      </button>
-      <input type="text" #username>
-      <div>{{ name }}</div>
+      <input 
+        type="text" 
+        [value]="name" 
+        (input)="handleChange($event.target.value)">
+      
+      <template [ngIf]="name.length > 2">
+        <div>
+          Searching for... {{ name }}
+        </div>
+      </template>
+
+      <div *ngIf="name.length > 2">
+        Searching for... {{ name }}
+      </div>
+
     </div>
   `
 })
 export class AppComponent {
-  name: string = 'Todd';
-  handleClick(value: string) {
-    console.log(value);
+  name: string = '';
+  handleChange(value: string) {
+    this.name = value;
   }
 }
