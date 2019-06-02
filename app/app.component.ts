b5c3bb5455a9088e7ce6196d78e99e18f1cb6126
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2019-06-01 17:41:39 -0700

    ngFor and interating collections

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,31 +1,53 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <input 
-        type="text" 
-        [value]="name" 
-        (input)="handleChange($event.target.value)">
-      
-      <template [ngIf]="name.length > 2">
-        <div>
-          Searching for... {{ name }}
-        </div>
-      </template>
-
-      <div *ngIf="name.length > 2">
-        Searching for... {{ name }}
-      </div>
-
+      <h3>Airline Passengers</h3>
+      <ul>
+        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
+          <li>
+            {{ i }}: {{ passenger.fullname }}
+          </li>
+        </template>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
     </div>
   `
 })
 export class AppComponent {
-  name: string = '';
-  handleChange(value: string) {
-    this.name = value;
-  }
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false
+  }];
 }
